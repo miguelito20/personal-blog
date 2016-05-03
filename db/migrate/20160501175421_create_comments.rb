ff9ada2d723faab0ca2d60eaeef7ea25920d67fa
Michael <lauerboy1@gmail.com>
2016-05-02 22:50:45 -0400

    adding profile and changing/ignoring migration for addition later due to foreign key constraints

--- db/migrate/20160501175421_create_comments.rb
+++ db/migrate/20160501175421_create_comments.rb
@@ -3,8 +3,8 @@ class CreateComments < ActiveRecord::Migration
     create_table :comments do |t|
       t.string :title
       t.text :body
-      t.references :user, index: true, foreign_key: true
-      t.references :article, index: true, foreign_key: true
+      t.belongs_to :user, index: true, foreign_key: true
+      t.belongs_to :article, index: true, foreign_key: true
       t.timestamps null: false
     end
   end
